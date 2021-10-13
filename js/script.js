81b3447444c1949412a5a0bcc30e0ae2101b0747
JuliaIvakh <85705848+JuliaIvakh@users.noreply.github.com>
2021-10-13 23:34:04 +0300

    hw12

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-let UserNum = +prompt("Enter any number");
-let numerator = 2;
-while (UserNum % numerator === 1) {
-  numerator = numerator + 1;
-  console.log(numerator);
+const userNum = +prompt("Enter any number");
+let result;
+
+for (let i = 2; i <= userNum; i++) {
+  if (userNum % i === 0) {
+    result = i;
+    break;
+  } else if (userNum < 1) {
+    result = NaN;
+  }
 }
-if (UserNum === numerator) {
-  numerator = `${UserNum} is a prime number, which is equal to the smallest numerator. `;
-  console.log(numerator);
-} else if (UserNum < 1) {
-  console.log(NaN);
-}
+console.log(+result);
